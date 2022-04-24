d267222d7efa7ad9fb661f0654ec5e6914ca997d
Zymberi <53353881+Zymberi@users.noreply.github.com>
2022-04-24 17:18:57 +0300

    Add files via upload

---
+++ ClockApp/src/main/java/com/mycompany/clockapp/Main.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.clockapp;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+
+        new MyFrame();
+        // TODO code application logic here
+    }
+    
+}
